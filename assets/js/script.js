750c7484031fe55f49bbe11b1da1c4984ec393b8
Giselle Souza <reallovetour@gmail.com>
2023-05-10 22:55:20 -0300

    funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,18 @@ let emailOk = false;
 
 
 function trocarFoto() {
-        let foto = document.querySelector("#foto").src;
-        console.log(foto)
-        let cardGoku1 = "https://www.cartonionline.com/wordpress/wp-content/uploads/2023/02/goku-814x1024.jpg"
-        let cardGoku2 = "https://i.pinimg.com/originals/df/74/95/df7495deca40e1039f00acb328338961.jpg";
+    let foto = document.querySelector("#foto").src;
+    console.log(foto)
+    let cardGoku1 = "https://www.cartonionline.com/wordpress/wp-content/uploads/2023/02/goku-814x1024.jpg"
+    let cardGoku2 = "https://i.pinimg.com/originals/df/74/95/df7495deca40e1039f00acb328338961.jpg";
 
-        if (foto == cardGoku2) {
-            document.getElementById('foto').src = cardGoku1;
-        }
-        if (foto == cardGoku1) {
-            document.getElementById('foto').src = cardGoku2;
-        }
-    
+    if (foto == cardGoku2) {
+        document.getElementById('foto').src = cardGoku1;
+    }
+    if (foto == cardGoku1) {
+        document.getElementById('foto').src = cardGoku2;
+    }
+
 }
 
 function validarNome() {
@@ -39,9 +39,23 @@ function validarNome() {
     }
 }
 
+function validarEmail() {
+    let txtEmail = document.querySelector("#txtEmail");
+
+    if (email.value.indexOf("@") === -1 || email.value.indexOf(".") === -1) {
+        txtEmail.innerHTML = "E-mail inválido";
+        txtEmail.style.color = "red";
+        emailOk = false;
+    } else {
+        txtEmail.innerHTML = "✔";
+        txtEmail.style.color = "green";
+        emailOk = true;
+    }
+}
+
 function validarEmailRegEx() {
 
-    let regex = /^\w+([.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let txtEmail = document.querySelector("#txtEmail");
 
     if (email.value.match(regex)) {
